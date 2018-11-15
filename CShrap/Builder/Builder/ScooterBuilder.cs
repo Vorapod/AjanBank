b0bc3705a68627ac11314efbe22f58b8a15e7a14
Toon <Toon@kobkiat-it.com>
2018-11-15 11:36:08 +0700

    delete using namespace

--- CShrap/Builder/Builder/ScooterBuilder.cs
+++ CShrap/Builder/Builder/ScooterBuilder.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Builder.Builder;
-
-namespace Builder.Builder
+﻿namespace Builder.Builder
 {
     /// <summary>
     /// The 'ConcreteBuilder3' class
